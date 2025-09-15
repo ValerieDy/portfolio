61258be28196cba35ac6f04482a796261485b20d
codecademy <ccuser@codecademy.com>
2025-09-15 22:06:20 +0000

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get '/welcome', to: 'pages#welcome', as: 'welcome'
 
   get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
 end
